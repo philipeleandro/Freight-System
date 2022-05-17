f976a97f1a1242b65f398df81123e348a6238543
Philipe <philipe-leandro@hotmail.com>
2022-05-17 20:40:40 -0300

    Create scenario test

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -15,6 +15,20 @@ describe 'User authentication' do
     expect(page).to have_button('Sair')
   end
 
+  it 'fail' do
+    User.create(name: 'Marcos', email: 'email@sistemadeentregas.com.br', password: 'password')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'email@sistemadeentregas.com'
+    fill_in 'Senha', with: '123547888'
+    within('form') do
+      click_on 'Entrar'
+    end
+
+    expect(current_path).to eq('/users/sign_in')
+  end
+
   it 'logout' do
     user = User.create(name: 'Marcos', email: 'email@sistemadeentregas.com.br', password: 'password')
 
