b04091aa1d8f254ea1929870a338667f6d62f343
Philipe <philipe-leandro@hotmail.com>
2022-05-19 10:29:21 -0300

    Add validation to Price

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,6 @@
 class Price < ApplicationRecord
   belongs_to :company
+
+  validates :min_volume, :max_volume, :min_weight, :max_weight, :value, presence: true
+  validates :min_volume, :max_volume, :min_weight, :max_weight, :value, numericality: { greater_than: 0 }
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'must have a min volume' do
+        admin = User.create(name: 'admin', email: 'admin@sistema0deentregas.com.br', password: '12345678')
+        company = Company.create(corporate_name: 'Impact', domain: 'impact.com.br', brand_name: 'Impact', address: 'Rua das flores, 1000', cnpj: '12345678974568', freight: 100, threshold: 500, user: admin)
+        user = User.create(name: 'Impact', email: 'user@impact.com.br', password: '12345678')
+        price = Price.create(min_volume: "", max_volume: 20, min_weight: 10, max_weight: 20, value: 50, company: company)
 
+        expect(price.valid?). to eq false
+      end
+
+      it 'must have a value' do
+        admin = User.create(name: 'admin', email: 'admin@sistema0deentregas.com.br', password: '12345678')
+        company = Company.create(corporate_name: 'Impact', domain: 'impact.com.br', brand_name: 'Impact', address: 'Rua das flores, 1000', cnpj: '12345678974568', freight: 100, threshold: 500, user: admin)
+        user = User.create(name: 'Impact', email: 'user@impact.com.br', password: '12345678')
+        price = Price.create(min_volume: 15, max_volume: 20, min_weight: 10, max_weight: 20, value: "", company: company)
+
+        expect(price.valid?). to eq false
+      end
+    end
+
+    context 'greater' do
+      it 'min_volue must be greater than 0' do
+        admin = User.create(name: 'admin', email: 'admin@sistema0deentregas.com.br', password: '12345678')
+        company = Company.create(corporate_name: 'Impact', domain: 'impact.com.br', brand_name: 'Impact', address: 'Rua das flores, 1000', cnpj: '12345678974568', freight: 100, threshold: 500, user: admin)
+        user = User.create(name: 'Impact', email: 'user@impact.com.br', password: '12345678')
+        price = Price.create(min_volume: 0, max_volume: 20, min_weight: 10, max_weight: 20, value: 50, company: company)
+
+        expect(price.valid?). to eq false
+      end
+
+      it 'value must be greater than 0' do
+        admin = User.create(name: 'admin', email: 'admin@sistema0deentregas.com.br', password: '12345678')
+        company = Company.create(corporate_name: 'Impact', domain: 'impact.com.br', brand_name: 'Impact', address: 'Rua das flores, 1000', cnpj: '12345678974568', freight: 100, threshold: 500, user: admin)
+        user = User.create(name: 'Impact', email: 'user@impact.com.br', password: '12345678')
+        price = Price.create(min_volume: 15, max_volume: 20, min_weight: 10, max_weight: 20, value: 0, company: company)
+
+        expect(price.valid?). to eq false
+      end
+    end
+  end
 end
