e0c4a0d36402d25e151afa89ea8ece54dbc4271a
Philipe <philipe-leandro@hotmail.com>
2022-05-19 17:27:59 -0300

    Add validations to car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :company
-
+  
   validates :brand, :model, :year, :max_load, :plate, presence: true
+  validates :year, numericality: { greater_than: 2000 }
+  validates :plate, format: { with: /[A-Z]{3}-[0-9]{4}/ }
+  validates :plate, length: { is: 8 }
+  validates :plate, uniqueness: true
 end
